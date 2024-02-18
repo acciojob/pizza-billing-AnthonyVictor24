7000db2fc4cc5bf97a78fa89239df713870d0b05
AnthonyVictor24 <arogyaanthonyvictor24@gmail.com>
2024-02-18 23:09:11 +0530

    Change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
     dp.addExtraCheese();
-    dp.addTakeaway();
+//    dp.addTakeaway();
     dp.addExtraCheese();
     dp.addExtraToppings();
     dp.addExtraCheese();
@@ -13,19 +13,20 @@ public class Main {
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(false);
-    p.addExtraCheese();
     p.addExtraToppings();
+    p.addExtraCheese();
     p.addExtraCheese();
     p.addExtraCheese();
     System.out.println(p.getBill());
 
     DeluxePizza dp2 = new DeluxePizza(false);
     dp2.addExtraCheese();
-    dp2.addExtraToppings();
+//    dp2.addExtraToppings();
+    dp2.addTakeaway();
     System.out.println(dp2.getBill());
 
     Pizza p2 = new Pizza(true);
-    p2.addExtraCheese();
+//    p2.addExtraCheese();
     p2.addExtraToppings();
     p2.addTakeaway();
     System.out.println(p2.getBill());
