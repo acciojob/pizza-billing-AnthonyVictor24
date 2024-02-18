94fe0871ea84efcc852e514facce49587e87e094
AnthonyVictor24 <arogyaanthonyvictor24@gmail.com>
2024-02-18 22:55:01 +0530

    changed ouput format

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,14 +1,6 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
-//    public DeluxePizza(Boolean isVeg) {
-//        super(isVeg);
-//        // your code goes here
-//
-//    }
-
-
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,32 +2,6 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-//    DeluxePizza dp = new DeluxePizza(true);
-//    dp.bill();
-//    System.out.println(dp.getPrice());
-////
-////    dp.addTakeaway();
-////    dp.addExtraCheese();
-////    dp.addTakeaway();
-//    System.out.println(dp.getBill());
-//
-//    System.out.println();
-//    System.out.println();
-//    Pizza p = new Pizza(false);
-////    p.addExtraCheese();
-////    p.addExtraToppings();
-////    p.addTakeaway();
-//    System.out.println(p.getBill());
-////    p.bill();
-//
-//    DeluxePizza dp1 = new DeluxePizza(false);
-//    System.out.println(dp1.getBill());
-
-
-
-
-
-
     DeluxePizza dp = new DeluxePizza(true);
     dp.addExtraCheese();
     dp.addTakeaway();
@@ -49,6 +23,13 @@ public class Main {
     dp2.addExtraCheese();
     dp2.addExtraToppings();
     System.out.println(dp2.getBill());
+
+    Pizza p2 = new Pizza(true);
+    p2.addExtraCheese();
+    p2.addExtraToppings();
+    p2.addTakeaway();
+    System.out.println(p2.getBill());
+
   }
 }
 
