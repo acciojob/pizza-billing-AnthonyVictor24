bc249170418b3c78040188dc501ad8eca3b56a29
AnthonyVictor24 <arogyaanthonyvictor24@gmail.com>
2024-02-18 23:03:09 +0530

    Small

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,8 +4,8 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        this.addExtraCheese = false;
-        this.addExtraTopping = false;
+//        this.addExtraCheese = false;
+//        this.addExtraTopping = false;
 //        if(isVeg == true){
 //            this.totalPrice +=70;
 //        }else{
