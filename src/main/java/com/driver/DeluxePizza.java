a185ce4f46673cb42f5cfbf29220488f2bf45bc3
AnthonyVictor24 <arogyaanthonyvictor24@gmail.com>
2024-02-19 18:04:23 +0530

    addd

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,6 +3,8 @@ package com.driver;
 public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        addExtraCheese();
+        addExtraToppings();
         // your code goes here
 //        this.addExtraCheese = false;
 //        this.addExtraTopping = false;
