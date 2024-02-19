d3008eba42b4f3c9c766febc176a37389a332d2e
AnthonyVictor24 <arogyaanthonyvictor24@gmail.com>
2024-02-19 18:47:49 +0530

    smalll

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,39 +3,21 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
+    dp.addExtraToppings();
     dp.addExtraCheese();
     dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addExtraToppings();
-    dp.addExtraCheese();
-    dp.addExtraToppings();
-
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(false);
-//    p.addExtraToppings();
-    p.addExtraCheese();
-    p.addExtraCheese();
-    p.addExtraCheese();
+    Pizza p = new Pizza(true);
+    p.addExtraToppings();
     p.addTakeaway();
     System.out.println(p.getBill());
 
-    DeluxePizza dp2 = new DeluxePizza(false);
-    dp2.addExtraCheese();
-    dp2.addExtraToppings();
-    dp2.addTakeaway();
-    System.out.println(dp2.getBill());
 
-    Pizza p2 = new Pizza(true);
-    p2.addExtraCheese();
-    p2.addExtraToppings();
-    p2.addTakeaway();
-    System.out.println(p2.getBill());
 
-    Pizza p3 = new Pizza(true);
-    p3.addExtraToppings();
-   p3.addTakeaway();
-    System.out.println(p3.getBill());
+
+
+
 
   }
 }
